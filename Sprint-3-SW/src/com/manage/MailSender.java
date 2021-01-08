1b92dfbe19f0a08a5615172fdf597820ff2e9735
ghada22 <66702956+ghada22@users.noreply.github.com>
2021-01-08 10:53:59 -0800

    Notification template and mail sender

---
+++ Sprint-3-SW/src/com/manage/MailSender.java
@@ -0,0 +1,42 @@
+package com.manage;
+
+import java.sql.SQLException;
+
+public class MailSender implements Sender{
+
+	private DataConnector data = new DataConnector();
+//	private DataConnector mails ;
+//	private DataConnector sms ;
+	
+	/**
+	 *	<h1>constructor</h1>
+	 *	a public constructor to be accessed in main method
+	 *	@throws ClassNotFoundException
+	 *  @throws SQLException
+	 */
+	public MailSender() throws ClassNotFoundException, SQLException{
+		data.connection = data.getConnection();
+	}
+	
+	
+	/**
+	 * <h1>send function</h1>
+	 * a function to mock of send an email
+	 * @param type,notificationType (which type stands for sms or mail , and notificationType stands for which type of notification wanna send)
+	 * @return true if notification sent false if not. 
+	 * @throws SQLException 
+	 */
+	@Override
+	public boolean send( String notificationType) throws SQLException {
+		if (MailConnector.types.contains(notificationType)) {
+				data.quarry = "DELETE FROM mails WHERE subject=?";
+				data.statement = data.connection.prepareStatement(data.quarry);
+				data.statement.setString(1, notificationType);
+				if (data.statement.execute()) {
+					return true;
+				}
+		}
+		return false;
+	} // end of function.
+
+}
---
+++ Sprint-3-SW/src/com/manage/NotificationModel.java
@@ -0,0 +1,25 @@
+package com.manage;
+
+public class NotificationModel {
+	 
+	int id;
+	String subject,content;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
